1b8ef19a7c84725f928e7c825ad18066343f31ea
Nohemi Morales <nohemi@umail.ucsb.edu>
2021-02-22 12:12:35 -0800

    V2

--- lecture2Practice/main.cpp
+++ lecture2Practice/main.cpp
@@ -5,15 +5,22 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <unordered_map
+#include <unordered_map>
 
 using namespace std;
 
 int main()
 {
     //Vectors
-
-
+    vector<int> f1;
+    
+    for (int i = 0; i <= 10; i++) {
+        f1.push_back(i);
+    }
+    cout << "Output begins here: " << endl;
+    for (auto i = f1.begin(); i != f1.end(); ++i) {
+        cout << *i << " ";
+    }
 
 
     return 0;
